609658b5fda895316ebd4cfcc2b46d3fbc583653
Chineek <350240550@qq.com>
2019-12-18 13:41:57 +0800

    更新了Paypal支付接口和跳转程序

--- litemall-vue/src/utils/request.js
+++ litemall-vue/src/utils/request.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
-import { Dialog, Toast } from 'vant';
+import {
+  Dialog,
+  Toast
+} from 'vant';
 
 // create an axios instance
 const service = axios.create({
@@ -9,7 +12,7 @@ const service = axios.create({
 
 // request interceptor
 service.interceptors.request.use(
-    config => {
+  config => {
     if (!config.headers['X-Litemall-Token']) {
       config.headers['X-Litemall-Token'] = `${window.localStorage.getItem(
         'Authorization'
@@ -24,20 +27,21 @@ service.interceptors.request.use(
 service.interceptors.response.use(
   response => {
     const res = response.data
-
     if (res.errno === 501) {
-        Toast.fail('请登录');
-        setTimeout(() => {
-          window.location = '#/login/'
-        }, 1500)
+      Toast.fail('请登录');
+      setTimeout(() => {
+        window.location = '#/login/'
+      }, 1500)
       return Promise.reject('error')
     } else if (res.errno === 502) {
-        Toast.fail('网站内部错误，请联系网站维护人员')
+      Toast.fail('网站内部错误，请联系网站维护人员')
       return Promise.reject('error')
-    } if (res.errno === 401) {
+    }
+    if (res.errno === 401) {
       Toast.fail('参数不对');
       return Promise.reject('error')
-    } if (res.errno === 402) {
+    }
+    if (res.errno === 402) {
       Toast.fail('参数值不对');
       return Promise.reject('error')
     } else if (res.errno !== 0) {
@@ -47,11 +51,11 @@ service.interceptors.response.use(
       return response
     }
   }, error => {
-    console.log('err' + error)// for debug
+    console.log('err' + error) // for debug
     Dialog.alert({
-        title: '警告',
-        message: '登录连接超时'
-      });
+      title: '警告',
+      message: '登录连接超时'
+    });
     return Promise.reject(error)
   })
 
